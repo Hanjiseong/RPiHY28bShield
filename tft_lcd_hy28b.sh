2cbb4289dec5a3c361794fab1054cf6f4510f4c0
Seungrok Han <muster22@naver.com>
2014-02-17 23:12:41 +0900

    Update tft_lcd_hy28b.sh

--- tft_lcd_hy28b.sh
+++ tft_lcd_hy28b.sh
@@ -15,14 +15,14 @@ sudo mv /lib/modules/$(uname -r) /lib/modules/$(uname -r).bak
 sudo REPO_URI=https://github.com/notro/rpi-firmware rpi-update
 
 # TFT LCD Module active
-sudo sed -i "a\modprobe fb_ili9325 dma" /etc/modules
-sudo sed -i "a\fbtft_device name=hy28b rotate=90 speed=48000000 fps=20" /etc/modules
-sudo sed -i "a\ads7846_device pressure_max=255 y_min=190 y_max=3850 gpio_pendown=17 x_max=3850 x_min=230 x_plate_ohms=100 swap_xy=1 verbose=3" /etc/modules
-sudo sed -i "a\ads7846" /etc/modules
+sudo sed -i '$a\modprobe fb_ili9325 dma' /etc/modules
+sudo sed -i '$a\fbtft_device name=hy28b rotate=90 speed=48000000 fps=20' /etc/modules
+sudo sed -i '$a\ads7846_device pressure_max=255 y_min=190 y_max=3850 gpio_pendown=17 x_max=3850 x_min=230 x_plate_ohms=100 swap_xy=1 verbose=3' /etc/modules
+sudo sed -i '$a\ads7846' /etc/modules
 
 # TFT-LCD enable
-sudo sed -i "a\con2fbmap 1 1" /etc/profile
-sudo sed -i "a\con2fbmap 1" /etc/profile
+sudo sed -i '$a\con2fbmap 1 1' /etc/profile
+sudo sed -i '$a\con2fbmap 1' /etc/profile
 
 #sudo nano /boot/cmdline.txt
 sudo sed -i "1s/rootwait/rootwait fbcon=map:10 fbcon=font:ProFont6x11/" /boot/cmdline.txt
