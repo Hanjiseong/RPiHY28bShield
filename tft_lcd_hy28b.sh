0c35a3c9bb09a929e407c4069ee3a37fc86917f1
rasplay <riskpw@nate.com>
2014-02-17 21:56:03 +0900

    Update tft_lcd_hy28b.sh

--- tft_lcd_hy28b.sh
+++ tft_lcd_hy28b.sh
@@ -15,8 +15,8 @@ sudo mv /lib/modules/$(uname -r) /lib/modules/$(uname -r).bak
 sudo REPO_URI=https://github.com/notro/rpi-firmware rpi-update
 
 # TFT LCD Module active
-sudo sed -i "a\modprobe fb_ili9320 dma" /etc/modules
-sudo sed -i "a\fbtft_device name=hy28a rotate=270 speed=48000000 fps=20" /etc/modules
+sudo sed -i "a\modprobe fb_ili9325 dma" /etc/modules
+sudo sed -i "a\fbtft_device name=hy28b rotate=90 speed=48000000 fps=20" /etc/modules
 sudo sed -i "a\ads7846_device pressure_max=255 y_min=190 y_max=3850 gpio_pendown=17 x_max=3850 x_min=230 x_plate_ohms=100 swap_xy=1 verbose=3" /etc/modules
 sudo sed -i "a\ads7846" /etc/modules
 
